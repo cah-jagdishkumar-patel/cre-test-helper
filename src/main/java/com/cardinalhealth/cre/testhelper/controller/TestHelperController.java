d8106775029d0bf3469fd67aa4f45b654372032f
Jagdish Patel <jagdishkumar.patel@cardinalhealth.com>
2023-01-18 10:00:51 -0500

    Initial Commit

--- src/main/java/com/cardinalhealth/cre/testhelper/controller/TestHelperController.java
+++ src/main/java/com/cardinalhealth/cre/testhelper/controller/TestHelperController.java
@@ -1,13 +1,22 @@
 package com.cardinalhealth.cre.testhelper.controller;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 
 @RestController
 public class TestHelperController {
@@ -22,5 +31,23 @@ public class TestHelperController {
     public void sendPRMessage(@RequestBody final String message) {
         queueMessagingTemplate.convertAndSend(prInputQueueName, message);
         LOGGER.info("PR message sent to queue: {}", prInputQueueName);
+    }
+
+    @GetMapping("/fakePriorServiceData")
+    public Map<String,Object> fakePriorServiceData(final String patientId) {
+        String data = "{}";
+        try {
+            Resource resource = new ClassPathResource(String.format("%s_FakePriorServiceData.json", patientId));
+            data = Files.readString(Path.of(resource.getURL().getPath()));
+        } catch (IOException e) {
+            final String errorMessage = String.format("error reading fake prior service data for %s", patientId);
+            LOGGER.error(errorMessage);
+        }
+        return new JSONObject(data).toMap();
+    }
+
+    @GetMapping("/fakeServiceData")
+    public String fakeServiceData(final String patientId, final String valueList) {
+        return "0.5";
     }
 }
--- src/main/java/com/cardinalhealth/cre/testhelper/sqs/receiver/NMOutputMessageReceiver.java
+++ src/main/java/com/cardinalhealth/cre/testhelper/sqs/receiver/NMOutputMessageReceiver.java
@@ -1,17 +1,19 @@
-package com.cardinalhealth.cre.testhelper.sqs.listener;
+package com.cardinalhealth.cre.testhelper.sqs.receiver;
 
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+@Service
 public class NMOutputMessageReceiver {
     private static final Logger LOGGER = LoggerFactory.getLogger(NMOutputMessageReceiver.class);
 
     @SqsListener(value="${cloud.aws.queue.nm.output.name}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    private void receiveNMOutputMessage(final String message) {
+    private void receiveMessage(final String message) {
         LOGGER.info("NM Output message received: {}", message);
         if(!StringUtils.hasText(message)) {
             final String errorMessage = "received empty SE Data Ready message";
