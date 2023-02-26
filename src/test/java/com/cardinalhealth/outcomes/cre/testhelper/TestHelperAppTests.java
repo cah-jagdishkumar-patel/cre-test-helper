fcf379670cabfa2f10bc9d13e0c446ef8d7d747d
Jagdish Patel <jagdishkumar.patel@cardinalhealth.com>
2023-02-25 20:50:00 -0500

    Initial Commit

--- src/test/java/com/cardinalhealth/outcomes/cre/testhelper/TestHelperAppTests.java
+++ src/test/java/com/cardinalhealth/outcomes/cre/testhelper/TestHelperAppTests.java
@@ -3,10 +3,8 @@ package com.cardinalhealth.outcomes.cre.testhelper;
 import com.cardinalhealth.outcomes.cre.testhelper.config.AppTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = AppTestConfig.class)
-@ActiveProfiles({"test"})
 class TestHelperAppTests {
 
     @Test
